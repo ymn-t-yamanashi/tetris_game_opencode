36eed11f0368bcfe9c3145235aea90269d34263e
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2026-03-03 21:51:39 +0900

    Fix GameLive layout rendering and remove unused .heex template

--- lib/tetris_game_opencode_web/live/game_live.ex
+++ lib/tetris_game_opencode_web/live/game_live.ex
@@ -1,7 +1,11 @@
 defmodule TetrisGameOpencodeWeb.GameLive do
   use TetrisGameOpencodeWeb, :live_view
+  alias TetrisGameOpencodeWeb.Layouts
+
   @impl true
   def render(assigns) do
+    assigns = Map.put_new(assigns, :flash, %{})
+
     ~H"""
     <Layouts.app flash={@flash} current_scope={@current_scope}>
       <div id="game-field">{@field.width} x {@field.height}</div>
@@ -9,12 +13,13 @@ defmodule TetrisGameOpencodeWeb.GameLive do
     """
   end
 
-  use Phoenix.LiveView
   alias TetrisGameOpencode.Tetris
 
   @impl true
-  def mount(_params, _session, socket) do
+  def mount(_params, session, socket) do
+    socket = assign(socket, :current_scope, Map.get(session, "current_scope"))
     if connected?(socket), do: Process.send_after(self(), :tick, 1000)
+
     field = Tetris.GameField.new()
     current_piece = Tetris.Tetromino.random()
 
@@ -29,14 +34,11 @@ defmodule TetrisGameOpencodeWeb.GameLive do
 
   @impl true
   def handle_info(:tick, socket) do
-    {:noreply,
-     socket
-     |> push_event("tick", %{})}
+    {:noreply, push_event(socket, "tick", %{})}
   end
 
   @impl true
   def handle_event("key", %{"code" => code}, socket) do
-    # simple left/right logic
     new_piece =
       case code do
         "ArrowLeft" -> Tetris.Tetromino.move_left(socket.assigns.current_piece)
